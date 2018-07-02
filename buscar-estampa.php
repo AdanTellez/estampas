5e88548229ff33432bca69190a8340a9ae83c271
Shiny Alex <carvantes.alex@gmail.com>
2018-07-02 03:44:22 -0500

    Agregué la búsqueda sin BD
    Ya busca con la cadena si la mandas bien, lo deje con una prueba local pero ya debe funcionar, solo sería ponerle el query y así.

--- buscar-estampa.php
+++ buscar-estampa.php
@@ -8,17 +8,51 @@
 //including the database connection file
 include_once("config.php");
 
-    $estampas = $_POST['estampas'];
+    //$estampas = $_POST['estampas'];
+    $estampas="16-123-8,12-66,3";  
     $estampas = trim($estampas);
     $tamaño   = strlen($estampas);
+    
+    print $estampas;
+    echo "<br>";
+    
+    $cont=0;
 
     for ($i = 0; $i <= $tamaño; $i++) {
-        print $estampas[$i];
+         //contador para cada estampa
+        $no_cantidad[$cont][1]=1; //para este arreglo, en su segunda dimensión, [0] es el no. de estampa
+        // y [1] es la cantidad de veces que se quiere dicha estampa.
+        
+        //print $estampas[$i];
+
+        if (is_numeric($estampas[$i])){
+                $no_cantidad[$cont][0].=$estampas[$i];
+            //.= append
+        }
+        else{
+            //si es ,
+            if($estampas[$i]==","){
+                $no_cantidad[$cont][1]="";
+                $i++;
+                    while (is_numeric($estampas[$i])){
+                        $no_cantidad[$cont][1].=$estampas[$i]; //número de veces que quiere la estampa
+                        $i++;
+                    } 
+            }
+            // cuando es -
+             $cont++;  
+        }
         
     }
+    
+    
+    echo '<pre>';
+		print_r($no_cantidad);
+    echo '</pre>';
+    
 
-    $result   = mysqli_query($mysqli, "SELECT no, id_album FROM estampa WHERE no='$no' and id_album='$id_album'");
-    $res      = mysqli_fetch_array($result);
+    //$result   = mysqli_query($mysqli, "SELECT no, id_album FROM estampa WHERE no='$no' and id_album='$id_album'");
+    //$res      = mysqli_fetch_array($result);
 
 ?>
 </body>
